a8e5ad88bc60c5d825ce7159334110ed00d43e31
Ash <ashloraptor@gmail.com>
2024-04-19 18:09:48 -0400

    Got browser page to load! Had to change auth.js.

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,91 +1,93 @@
-const { GraphQLError } = require('graphql');
-const jwt = require('jsonwebtoken');
-
-const secret = 'mysecretssshhhhhhh';
-const expiration = '2h';
-// use this to decode a token and get the user's information out of it
 import decode from 'jwt-decode';
 
-module.exports = {
-    AuthenticationError: new GraphQLError('Could not authenticate user.', {
-      extensions: {
-        code: 'UNAUTHENTICATED',
-      },
-    }),
-    authMiddleware: function ({ req }) {
-      // allows token to be sent via req.body, req.query, or headers
-      let token = req.body.token || req.query.token || req.headers.authorization;
+// const { GraphQLError } = require('graphql');
+// const jwt = require('jsonwebtoken');
+
+// const secret = 'mysecretssshhhhhhh';
+// const expiration = '2h';
+// // // use this to decode a token and get the user's information out of it
+// // import decode from 'jwt-decode';
+
+// module.exports = {
+//     AuthenticationError: new GraphQLError('Could not authenticate user.', {
+//       extensions: {
+//         code: 'UNAUTHENTICATED',
+//       },
+//     }),
+//     // authMiddleware: function ({ req }) {
+//     //   // allows token to be sent via req.body, req.query, or headers
+//     //   let token = req.body.token || req.query.token || req.headers.authorization;
   
-      // We split the token string into an array and return actual token
-      if (req.headers.authorization) {
-        token = token.split(' ').pop().trim();
-      }
+//     //   // We split the token string into an array and return actual token
+//     //   if (req.headers.authorization) {
+//     //     token = token.split(' ').pop().trim();
+//     //   }
   
-      if (!token) {
-        return req;
-      }
+//     //   if (!token) {
+//     //     return req;
+//     //   }
   
-      // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
-      try {
-        const { data } = jwt.verify(token, secret, { maxAge: expiration });
-        req.user = data;
-      } catch {
-        console.log('Invalid token');
-      }
+//     //   // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
+//     //   try {
+//     //     const { data } = jwt.verify(token, secret, { maxAge: expiration });
+//     //     req.user = data;
+//     //   } catch {
+//     //     console.log('Invalid token');
+//     //   }
   
-      // return the request object so it can be passed to the resolver as `context`
-      return req;
-    },
-    signToken: function ({ email, name, _id }) {
-      const payload = { email, name, _id };
-      return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-    },
-  };
+//     //   // return the request object so it can be passed to the resolver as `context`
+//     //   return req;
+//     // },
+//     signToken: function ({ email, name, _id }) {
+//       const payload = { email, name, _id };
+//       return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+//     },
+//   };
   
 
-// // create a new class to instantiate for a user
-// class AuthService {
-//   // get user data
-//   getProfile() {
-//     return decode(this.getToken());
-//   }
+// create a new class to instantiate for a user
+class AuthService {
+  // get user data
+  getProfile() {
+    return decode(this.getToken());
+  }
 
-//   // check if user's logged in
-//   loggedIn() {
-//     // Checks if there is a saved token and it's still valid
-//     const token = this.getToken();
-//     return !!token && !this.isTokenExpired(token); // handwaiving here
-//   }
+  // check if user's logged in
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token); // handwaiving here
+  }
 
-//   // check if token is expired
-//   isTokenExpired(token) {
-//     try {
-//       const decoded = decode(token);
-//       if (decoded.exp < Date.now() / 1000) {
-//         return true;
-//       } else return false;
-//     } catch (err) {
-//       return false;
-//     }
-//   }
+  // check if token is expired
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
 
-//   getToken() {
-//     // Retrieves the user token from localStorage
-//     return localStorage.getItem('id_token');
-//   }
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
+  }
 
-//   login(idToken) {
-//     // Saves user token to localStorage
-//     localStorage.setItem('id_token', idToken);
-//     window.location.assign('/');
-//   }
+  login(idToken) {
+    // Saves user token to localStorage
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
 
-//   logout() {
-//     // Clear user token and profile data from localStorage
-//     localStorage.removeItem('id_token');
-//     // this will reload the page and reset the state of the application
-//     window.location.assign('/');
-//   }
-// }
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
+}
 
 export default new AuthService();
