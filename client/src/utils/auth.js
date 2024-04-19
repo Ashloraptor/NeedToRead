aaeed216062009f2d03b9ef9b5d774b8973a3b21
Ash <ashloraptor@gmail.com>
2024-04-19 17:18:42 -0400

    Added Apollo/Client, made changes to debug

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -88,4 +88,4 @@ module.exports = {
 //   }
 // }
 
-// export default new AuthService();
+export default new AuthService();
--- server/server.js
+++ server/server.js
@@ -22,16 +22,16 @@ const server = new ApolloServer({
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
