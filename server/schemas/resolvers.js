b22c0236b33dbe1e41db1f908e7c075b63ea8c8c
Ash <ashloraptor@gmail.com>
2024-04-23 10:14:44 -0400

    SignUp and Login are both successful!

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,13 +5,19 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find({});
+      return User.find().populate('books');
     },
     //savedBooks, or books?
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('books');
+    },
     books: async (parent, { _id }) => {
       const params = _id ? { _id } : {};
       return Book.find(params);
     },
+    book: async (parent, { _id }) => {
+      return Book.findOne({ _id: bookId });
+    }, //unsure
   },
   Mutation: {
     addUser: async (parent, args) => {
@@ -20,7 +26,24 @@ const resolvers = {
       const token = signToken(user);
 
       return {token, user};
-    }
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
   }
 };
 
